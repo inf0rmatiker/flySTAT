4e38e64c1abd8d7ba3ec31dd5a196255a0530284
Caleb <ccarlson355@gmail.com>
2019-11-25 20:50:30 -0700

    Attempt to test spark-submit

--- core/src/main/scala/Main.scala
+++ core/src/main/scala/Main.scala
@@ -1,8 +1,18 @@
+import scala.util.{Try, Success, Failure}
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+
 object Main {
 
   def main(args: Array[String]): Unit = {
+    var conf: SparkConf = new SparkConf().setAppName("flySTAT")
+    var sc: SparkContext = new SparkContext(conf)
+
+    
     println("Hello, World")
-  
+
+
   }
 
 
