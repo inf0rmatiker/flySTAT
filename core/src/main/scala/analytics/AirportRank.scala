b5bf2380e42732b7a254b878a1d31a0eac9f891c
Caleb <ccarlson355@gmail.com>
2019-11-28 14:27:25 -0700

    Refactor misleading function

--- core/src/main/scala/analytics/AirportRank.scala
+++ core/src/main/scala/analytics/AirportRank.scala
@@ -4,6 +4,7 @@ import scala.util.{Try, Success, Failure}
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
+import org.apache.spark.rdd.RDD
 import scala.collection.mutable.ArrayBuffer
 import java.lang.Double
 
@@ -15,19 +16,18 @@ class AirportRank (inputFileNames: ArrayBuffer[String], appName: String, inputDi
 
   def calculateAirportRanks(): Unit = {
     var records    = files.filter(line => !line.contains("DOT_ID_Reporting_Airline")).map(line => line.split(","))
-    var airportIds = records.map(record => record(11)).distinct
-    var airportCount = airportIds.count().toInt
-
+  
+    val airportCount = getAirportCount(records)
     var ranks: Array[Double] = Array.fill(airportCount){ 1.0 / airportCount }
     sc.parallelize(ranks, 1).saveAsTextFile(outputDirectory)
+  
   }
 
     
-  def getAirportCount(): Long =  {
-    var totalCount: Long = 0
-    val files = sc.textFile(inputDirectory)
-    totalCount += files.count()
-    return totalCount
+  def getAirportCount(records: RDD[Array[String]] ): Int = {
+    var airportIds = records.map(record => record(11)).distinct
+    var airportCount = airportIds.count().toInt
+    return airportCount
   }
 
 }
