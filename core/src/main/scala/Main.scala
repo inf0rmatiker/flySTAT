463178c47d55f50f05249635e3956e520153e23f
Caleb <ccarlson355@gmail.com>
2019-11-28 11:45:20 -0700

    Collect list of input filenames, configure usage parameters

--- core/src/main/scala/Main.scala
+++ core/src/main/scala/Main.scala
@@ -2,18 +2,51 @@ import scala.util.{Try, Success, Failure}
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.{FileSystem, Path}
+import scala.collection.mutable.ArrayBuffer
 
 object Main {
 
   def main(args: Array[String]): Unit = {
-    var conf: SparkConf = new SparkConf().setAppName("flySTAT")
-    var sc: SparkContext = new SparkContext(conf)
+    if (args.length >=  2) {
+      var conf: SparkConf = new SparkConf().setAppName("flySTAT")
+      var sc: SparkContext = new SparkContext(conf)
 
-    
-    println("Hello, World")
-
-
+      val inputDirectory : String = args(0)
+      val outputDirectory: String = args(1)
+      
+      var fileNames: ArrayBuffer[String] = getFileNames(inputDirectory)
+      sc.parallelize(fileNames, 1).saveAsTextFile(outputDirectory)
+    }
+    else {
+      printUsageMessage()
+    }
   }
+
+  /**
+   * Input: A String containing the HDFS directory name.
+   * Output: ArrayBuffer[String] containing all the files in the HDFS directory.
+   *
+   * Example found at: https://stackoverflow.com/questions/33394884/spark-scala-list-folders-in-directory 
+   */
+  def getFileNames(directory: String): ArrayBuffer[String] = {
+    var fileNames: ArrayBuffer[String] = ArrayBuffer[String]()
+      
+    val fs = FileSystem.get(new Configuration())
+    val status = fs.listStatus(new Path(directory))
+    status.foreach( fileStatus => fileNames += fileStatus.getPath().getName() )
+    return fileNames
+  }
+
+
+  def printUsageMessage(): Unit = {
+    println("\nUsage:\n\n" +
+            "$\t${SPARK_HOME}/bin/spark-submit  \\\n" +
+            "\t\t--class Main \\\n" +
+            "\t\t--master <spark_master>  \\\n" +
+            "\t\t--supervise <jar_file> <hdfs_input_dir> <hdfs_output_dir>\n")
+    }
 
 
 }
