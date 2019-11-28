19aa1a932dd788ba2e13bbbf816e38befdde6f23
Caleb <ccarlson355@gmail.com>
2019-11-28 14:10:45 -0700

    Get initial rank vector for unique airports

--- core/src/main/scala/analytics/AirportRank.scala
+++ core/src/main/scala/analytics/AirportRank.scala
@@ -5,14 +5,21 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 import scala.collection.mutable.ArrayBuffer
+import java.lang.Double
 
 class AirportRank (inputFileNames: ArrayBuffer[String], appName: String, inputDirectory: String, outputDirectory: String) {
   var conf: SparkConf = new SparkConf().setAppName(appName)
   var sc: SparkContext = new SparkContext(conf)
 
+  val files = sc.textFile(inputDirectory)
+
   def calculateAirportRanks(): Unit = {
-    val lineCount: Long = getAirportCount()
-    sc.parallelize(Seq(lineCount), 1).saveAsTextFile(outputDirectory)
+    var records    = files.filter(line => !line.contains("DOT_ID_Reporting_Airline")).map(line => line.split(","))
+    var airportIds = records.map(record => record(11)).distinct
+    var airportCount = airportIds.count().toInt
+
+    var ranks: Array[Double] = Array.fill(airportCount){ 1.0 / airportCount }
+    sc.parallelize(ranks, 1).saveAsTextFile(outputDirectory)
   }
 
     
