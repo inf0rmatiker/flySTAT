6cd5338092ee331a8ad629970dfe4709cb240173
Caleb <ccarlson355@gmail.com>
2019-12-03 20:18:47 -0700

    Get daily averages for past 9 years

--- core/run.sh
+++ core/run.sh
@@ -8,13 +8,15 @@ if [[ $# -ge 2 ]]; then
   echo -e "\nRemoving any previously existing output dirs..."
   ${HADOOP_HOME}/bin/hdfs dfs -rm -r -f $2
 
+  #--executor-cores 10  \
+  #  --num-executors 10 \
+  #  --executor-memory 10g  \
+
+
   echo -e "\nSubmitting job to Spark..."
   ${SPARK_HOME}/bin/spark-submit  \
     --class Main  \
     --master spark://dover:34567  \
-    --executor-cores 10  \
-    --num-executors 10 \
-    --executor-memory 10g  \
     --supervise target/scala-2.11/flystat_2.11-1.0.jar $1 $2
 
 else
--- core/src/main/scala/analytics/DelayStats.scala
+++ core/src/main/scala/analytics/DelayStats.scala
@@ -11,7 +11,6 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.DataFrame
 
 import scala.collection.mutable.ArrayBuffer
-import java.lang.Double
 
 class DelayStats (inputFileNames: ArrayBuffer[String], appName: String, inputDirectory: String, outputDirectory: String) {
   val spark = SparkSession.builder.appName("FlySTAT").getOrCreate()
@@ -32,11 +31,15 @@ class DelayStats (inputFileNames: ArrayBuffer[String], appName: String, inputDir
                                                                     ((row(0).toString,row(1).toString),
                                                                      Array(row(1).toString,row(2).toString,row(3).toString,row(4).toString) ))
     
+    // Filter out any records for 2019, to test against
+    val subset: RDD[((String,String), Array[String])] = formattedData.filter( record =>
+                                                              record._1._2.contains("2019") )
+    
     // In the form ( (OriginAirportID, FlightDate), Iterable([FlightDate, DepDelayMinutes, ArrDelayMinutes, DestAirportID], [...], ...) )
-    var groupedByKey: RDD[((String,String), Iterable[Array[String]])] = formattedData.groupByKey()
+    var groupedByKey: RDD[((String,String), Iterable[Array[String]])] = subset.groupByKey()
     
     // In the form ( (OriginAirportID, FlightDate), AverageDelayMinutes )
-    var dailyAverageDelays = groupedByKey.map{ case(key, values) =>
+    var dailyAverageDelays: RDD[((String,String), Double)] = groupedByKey.map{ case(key, values) =>
       var count: Int = 0
       var totalSum: Double = 0.0
       
@@ -47,7 +50,13 @@ class DelayStats (inputFileNames: ArrayBuffer[String], appName: String, inputDir
       (key, totalSum / count)
     }
     
-    dailyAverageDelays.saveAsTextFile(outputDirectory) 
+    var yearlyAveragesByDay: RDD[(String, Iterable[(String, Double)])] = dailyAverageDelays.map{ case((airportID,date), averageDelay) =>
+                                                          ((airportID,date.substring(5, date.length())), averageDelay) }
+                                                        .mapValues((_,1)) // Add a "1" to the value tuples for counting
+                                                        .reduceByKey( (first,second) => (first._1 + second._1, first._2 + second._2)) // Sum delays and counts
+                                                        .mapValues{ case (sum, count) => (1.0 * sum) / count } // Transform to delay averages
+                                                        .map( record => (record._1._2, (record._1._1,record._2)) ) // Make the month and day the key
+                                                        .groupByKey().sortByKey()
     
   }
 
