1462dfac7d82ca2380d93ea8219d02e302bd7b09
Cassidy Skorczewski <cassidy.skor@gmail.com>
2019-12-04 19:30:33 -0700

    god bless america

--- core/src/main/scala/analytics/AllCities.scala
+++ core/src/main/scala/analytics/AllCities.scala
@@ -12,7 +12,7 @@ import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
 
 //class FiveNumberSummary (){
-object FiveNumberSummary {
+object AllCities {
     def main(args: Array[String]){
     val spark = SparkSession.builder.appName("FlySTAT").getOrCreate()
     val sc = SparkContext.getOrCreate()
@@ -20,20 +20,33 @@ object FiveNumberSummary {
     import sqlContext.implicits._
     
     //def AtoBYearData(): Unit = {
+    
         //Read in the data from HDFS as csv
         val data: DataFrame = spark.read.format("csv").option("header", "true").load("hdfs://cheyenne:30241/cs435/flySTAT/data/On_Time_Reporting_Carrier_On_Time_Performance_(1987_present)_2018_*.csv")
         
-        //Filter data so we are only looking origin and destination airports Denver and Chicago currently
-        val filteredData: Dataset[Row] = data.filter($"OriginAirportID" === 11292).filter($"DestAirportID" === 13930)
+        //Filter data so we are only looking origin airport Denver on August 8, 2014
+        val filteredData: Dataset[Row] = data.filter($"OriginAirportID" === 11292).filter($"FlightDate" === "2018-11-20")
         
-        //We only need Origin Airport, Destination Airport, Date, Delayed Minutes 
-        val relevantData: RDD[Row] = filteredData.select("FlightDate", "DepDelayMinutes").na.drop().rdd
+        //Keep only destination airport and delay minutes columns
+        val relevantData: RDD[Row] = filteredData.select("DestAirportID", "DepDelayMinutes").na.drop().rdd
         
-        //Group the sorted delays by date
-        val delaysPerDate: RDD[ (String, Iterable[Double])] = relevantData.map( row => (row(0).toString, row(1).toString.toDouble) ).groupByKey().map{ case(k,v) => (k, v.toArray.sorted) }
+        //Take the top 25 
+        val top25Dest: Array[Int] = relevantData.map( row => ( row(0).toString.toInt, 1 )).reduceByKey(_ + _).sortBy(_._2, false, 1).zipWithIndex.filter{ case (_, index) => (index < 50)}.keys.keys.collect
+
+        //Filter relevantData to only have our top 25 destination airports
+        val top25Data: RDD[Row] = relevantData.filter{ row => top25Dest.contains(row(0).toString.toInt) }
         
+        //Group the sorted delays by airport
+         val delaysPerAirportID: RDD[ (Int, Iterable[Double])] = top25Data.map( row => (row(0).toString.toInt, row(1).toString.toDouble) ).groupByKey().map{ case(k,v) => (k, v.toArray.sorted) }
+         
+         //Read in the airport names
+         val airportID: RDD[(Int, String)] = spark.read.format("csv").option("header", "true").load("hdfs://cheyenne:30241/cs435/flySTAT/lookup/airportID.csv").map( row => (row(0).toString.toInt, row(1).toString) ).filter( x => top25Dest.contains(x._1) ).rdd
+         
+         //Join and keep the airport name instead of airport id
+         val delaysPerAirport: RDD[(String, Iterable[Double] )] = delaysPerAirportID.join( airportID ).map{ case(id, (delays, name) ) => (name, delays)}
+         
         //Calculate the stats
-        val stats = delaysPerDate.map { case(date, delays) => 
+        val stats = delaysPerAirport.map { case(airport, delays) => 
                 val min = delays.min
                 val max = delays.max
                 val Q1 = delays.slice((delays.size * .25).floor.toInt, (delays.size * .25).floor.toInt + 1).toList.head
@@ -43,17 +56,17 @@ object FiveNumberSummary {
                 val count = delays.size
                 val outlierThreshold = Q3 + 1.5 * (Q3 - Q1)
                 val outliers = delays.filter(x => x > outlierThreshold).mkString(" ")
-                (date, (min, max, median, Q1, Q3, avg, count, outlierThreshold, outliers ) ) }
+                (airport, (min, max, median, Q1, Q3, avg, count, outlierThreshold, outliers ) ) }
     
         //Save to HDFS
-        stats.coalesce(1).saveAsTextFile("hdfs://nashville:30841/cs435/flySTAT/statWithZero")
+        stats.coalesce(1).saveAsTextFile("hdfs://nashville:30841/cs435/flySTAT/statWithZeroAirport2")
         
         //Collect the same stats but this time only do delays > 0
-        val nonZeroDelaysPerDate: RDD[(String, Array[Double])] = delaysPerDate.map { case(k,v) => (k, v.filter(x => x > 0).toArray ) }
+        val nonZeroDelaysPerDate: RDD[(String, Array[Double])] = delaysPerAirport.map { case(k,v) => (k, v.filter(x => x > 0).toArray ) }
         
-        val nonZeroStats = nonZeroDelaysPerDate.map { case(date, delays) =>
+        val nonZeroStats = nonZeroDelaysPerDate.map { case(airport, delays) =>
                 if(delays.size == 0 ){
-                    (date, (0, 0, 0, 0, 0, 0, 0, 0, "" ) )
+                    (airport, (0, 0, 0, 0, 0, 0, 0, 0, "" ) )
                 } else {
                     val min = delays.min
                     val max = delays.max
@@ -64,11 +77,11 @@ object FiveNumberSummary {
                     val count = delays.size
                     val outlierThreshold = Q3 + 1.5 * (Q3 - Q1)
                     val outliers = delays.filter(x => x > outlierThreshold).mkString(" ")
-                    (date, (min, max, median, Q1, Q3, avg, count, outlierThreshold, outliers ) )
+                    (airport, (min, max, median, Q1, Q3, avg, count, outlierThreshold, outliers ) )
                     }
                 }
                 
-        nonZeroStats.coalesce(1).saveAsTextFile("hdfs://nashville:30841/cs435/flySTAT/statWithoutZero")
+        nonZeroStats.coalesce(1).saveAsTextFile("hdfs://nashville:30841/cs435/flySTAT/statWithoutZeroAirport2")
         
     }
 }
