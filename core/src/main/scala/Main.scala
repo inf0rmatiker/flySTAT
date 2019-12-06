7dcd2fe1d0f76c37ace89e2603e0b15066632c02
Caleb Carlson <inf0rmatiker@users.noreply.github.com>
2019-12-05 22:47:36 -0700

    Inf0rmatiker averages (#6)
    * Add DelayStats class, rework strategy
    
    * Get averages for Jan1,2009
    
    * Refactor DelayStats
    
    * Get daily averages for past 9 years
    
    * Add transformations to see average accuracy
    
    * Get difference in predictions from averages
    
    * Get analytics for DEN avg predictions
    
    * Get total averages across entire dataset

--- core/src/main/scala/Main.scala
+++ core/src/main/scala/Main.scala
@@ -6,6 +6,7 @@ import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.{FileSystem, Path}
 import scala.collection.mutable.ArrayBuffer
 
+import analytics.DelayStats
 import analytics.AirportRank
 
 object Main {
@@ -16,8 +17,8 @@ object Main {
       val outputDirectory: String = args(1)
       
       var fileNames: ArrayBuffer[String] = getFileNames(inputDirectory) 
-      var airportRanks = new AirportRank(fileNames, "flySTAT", inputDirectory, outputDirectory)
-      airportRanks.calculateAirportRanks()
+      var delayStats = new DelayStats(fileNames, "flySTAT", inputDirectory, outputDirectory)
+      delayStats.predictDelays()
     }
     else {
       printUsageMessage()
