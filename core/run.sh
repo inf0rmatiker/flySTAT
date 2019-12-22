00e1a53a3fc6ee05a03970d57abb3168a0e3b017
Caleb <ccarlson355@gmail.com>
2019-12-22 16:18:52 -0700

    Fix run script

--- core/run.sh
+++ core/run.sh
@@ -1,24 +1,50 @@
 #!/bin/bash
 
-if [[ $# -ge 2 ]]; then
+function printUsageMessage() {
+  usageMessage="Usage:\n\n$\t./run.sh [OPTION] [ARGS]\n\n\tOPTION:\t\tDESCRIPTION:\n\n\t--averages, -a\tRuns average predictions.\n
+                \t\t\tArguments: <HDFS_Input_Path> <HDFS_Output_Path>\n\n
+                \t--linear, -l\tRuns linear regression.\n"
 
+  echo -e $usageMessage
+}
+
+function setup() {
   echo -e "\nRebuilding package..."
   sbt clean package
 
-  echo -e "\nRemoving any previously existing output dirs..."
-  ${HADOOP_HOME}/bin/hdfs dfs -rm -r -f $2
+  echo -e "\nRemoving any previously existing output directory..."
+  ${HADOOP_HOME}/bin/hdfs dfs -rm -r -f $1
+}
 
-  #--executor-cores 10  \
-  #  --num-executors 10 \
-  #  --executor-memory 10g  \
+if [[ $# -ge 3 ]]; then
+  
+  HDFS_INPUT_DIR=$2
+  HDFS_OUTPUT_DIR=$3
 
+  if [[ $1 == "--averages" || $1 == "-a" ]]; then
 
-  echo -e "\nSubmitting job to Spark..."
-  ${SPARK_HOME}/bin/spark-submit  \
-    --class Main  \
-    --master spark://dover:34567  \
-    --supervise target/scala-2.11/flystat_2.11-1.0.jar $1 $2
+    setup $HDFS_OUTPUT_DIR
 
-else
-  echo -e "Usage:\n\n$\t./run.sh <hdfs_input_path> <hdfs_output_path>\n"
+    echo -e "\nSubmitting averages job to Spark..."
+    ${SPARK_HOME}/bin/spark-submit  \
+      --class Main  \
+      --master spark://dover:34567  \
+      --supervise target/scala-2.11/flystat_2.11-1.0.jar $HDFS_INPUT_DIR $HDFS_OUTPUT_DIR 
+   
+  elif [[ $# -eq 5 && ($1 == "--linear" || $1 == "-l") ]]; then
+    
+    DATES_FILE=$4
+    AIRPORT_ID=$5 # I.E. DEN airport ID: 11292 TODO: Use lookup tables to search for name
+
+    setup $HDFS_OUTPUT_DIR
+
+    for date in $(cat < $DATES_FILE); do
+      $SPARK_HOME/bin/spark-submit --master spark://dover:34567 --class analytics.LinearRegression --supervise target/scala-2.11/flystat_2.11-1.0.jar $date $HDFS_OUTPUT_DIR $AIRPORT_ID
+    done   
+
+  else
+    printUsageMessage
+  fi
+else 
+  printUsageMessage
 fi
